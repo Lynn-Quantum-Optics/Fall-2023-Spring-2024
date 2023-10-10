01cdda404b7dcc9c2fc9f81e5e4158c036714236
Oscar Scholin <orsa2020@mymail.pomona.edu>
2023-10-10 04:32:11 -0700

    added parallelization

--- oscar/usplore_np.py
+++ oscar/usplore_np.py
@@ -83,12 +83,14 @@ def convert_kets(sig, d):
     return ket
 
 
-def get_all_signatures(d, b = True, display=False):
+def get_all_signatures(d, b = True, display=False, ret_len=True):
     '''Calls get_signature for all bell states'''
     signatures = []
     cp= []
     for c in range(d):
         for p in range(d):
+            if display:
+                print(f'(c,p) = ({c},{p})')
             sig = get_signature(d, c, p, b=b)
 
             # apply b/f statistics---------
@@ -141,14 +143,16 @@ def get_all_signatures(d, b = True, display=False):
         for existing_row in unique_signatures:
             if np.allclose(row, existing_row, atol=10**(-10)):
                 is_duplicate = True
-                print('----')
-                print('signatures unique', unique_signatures)
-                print('redundant state found', row)
-                print('----')
+                if display:
+                    print('----')
+                    print('signatures unique', unique_signatures)
+                    print('redundant state found', row)
+                    print('----')
                 break
             else:
-                print('should be unique', row)
-                print('existing row', existing_row)
+                if display:
+                    print('should be unique', row)
+                    print('existing row', existing_row)
         
         if not is_duplicate:
             unique_signatures.append(row)
@@ -192,31 +196,130 @@ def get_all_signatures(d, b = True, display=False):
 
     sys.stdout = original_stdout  # Reset stdout back to the original
             
-    return signatures
+    if ret_len:
+        return len(signatures)
+    else:
+        return signatures
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
-    num_sig_ls = []
-    min_d = 2
-    max_d = 20
-    for d in range(min_d, max_d+1):
-        for stat in [True]:
-            sig = len(get_all_signatures(d, display=True, b=stat))
-            print(f'd = {d}, b = {stat}, sig = {sig}')
-            num_sig_ls.append(sig)
+    from multiprocessing import Pool, cpu_count
+    from functools import partial
+    import os
 
-    num_sig_ls = np.array(num_sig_ls)
-    print(num_sig_ls)
-    np.save(f'output/num_sig_{min_d}_{max_d}.npy', num_sig_ls)
+    def compute_num_sig(min_d, max_d, b=True, parallel=True, display=False):
+        '''Compute the number of signatures for all d in range(min_d, max_d+1) and save to file.
+
+        Parameters:
+        min_d (int): minimum d value
+        max_d (int): maximum d value
+        b (bool): bosonic or fermionic statistics
+        parallel (bool): use multiprocessing or not
+        
+        '''
+        num_sig_ls = []
+  
+        # do multiprocessing
+        if parallel:
+            func = partial(get_all_signatures, b=b, display=display, ret_len=True)
+            with Pool(cpu_count()) as po:
+                num_sig_ls = po.map_async(func, range(min_d, max_d+1)).get()
+
+        # do serial
+        else:
+            for d in range(min_d, max_d+1):
+                for stat in [True]:
+                    sig = get_all_signatures(d, display=display, b=stat, ret_len=True)
+                    print(f'd = {d}, b = {stat}, sig = {sig}')
+                    num_sig_ls.append(sig)
+                    np.save(f'output/num_sig_{min_d}_{max_d}.npy', num_sig_ls)
+
+        num_sig_ls = np.array(num_sig_ls)
+        print(num_sig_ls)
+        np.save(f'output/num_sig_{min_d}_{max_d}.npy', num_sig_ls, allow_pickle=True)
+
+    def plot(min_d_max_d_ls):
+        '''Plot results of get_all_signatures given list of tuples (min_d, max_d)'''
+
+        min_min_d = min([min_d for min_d, max_d in min_d_max_d_ls])
+        max_max_d = max([max_d for min_d, max_d in min_d_max_d_ls])
+
+        fig, ax = plt.subplots(2, 1, figsize=(10,10))
+
+        for i, (min_d, max_d) in enumerate(min_d_max_d_ls):
+            num_sig_ls = np.load(f'output/num_sig_{min_d}_{max_d}.npy')
+            # determine the max power of 2 to use in pisenti comparison
+            min_2 = int(np.emath.logn(2, min_d))
+            max_2 = int(np.emath.logn(2, max_d))
+            max_2+=1
 
 
-    plt.figure(figsize=(7,7))
-    plt.scatter(np.arange(min_d, max_d+1, 1), num_sig_ls)
-    plt.xlabel('Dimension of Entanglement')
-    plt.ylabel('Number of Unique Signatures')
-    plt.title('LELM Distinguishability with QFT')
-    plt.tight_layout()
-    plt.savefig(f'output/num_sig_{min_d}_{max_d}.pdf')
+            if i ==0:
+                # plot number distinguished
+                ax[0].scatter(np.arange(min_d, max_d+1, 1), num_sig_ls, label='This work', color='blue')
+                # plot even answer: for d = 2^n, 2^{n+1}-1 unique states
+                ax[0].scatter(2**(np.arange(min_2, max_2,1)), 2**(np.arange(min_2, max_2,1)+1)-1, label='Pisenti et al. 2011', color='red')
+                ax[0].scatter([3], [3], label='Leslie et al. 2019', color='orange')
+        
+                # plot percent distinguished
+                ax[1].scatter(np.arange(min_d, max_d+1, 1), num_sig_ls/(np.arange(min_d, max_d+1, 1)**2), label='This work', color='blue')
+                ax[1].scatter(2**(np.arange(min_2, max_2,1)), (2**(np.arange(min_2, max_2,1)+1)-1) / (2**(np.arange(min_2, max_2,1)))**2, label='Pisenti et al. 2011', color='red')
+                ax[1].scatter([3], [3/9], label='Leslie et al. 2019', color='orange')
+            else:
+                # same as above but without labels
+                ax[0].scatter(np.arange(min_d, max_d+1, 1), num_sig_ls, color='blue')
+                ax[0].scatter(2**(np.arange(min_2, max_2,1)), 2**(np.arange(min_2, max_2,1)+1)-1, color='red')
+                ax[0].scatter([3], [3], color='orange')
+
+                ax[1].scatter(np.arange(min_d, max_d+1, 1), num_sig_ls/(np.arange(min_d, max_d+1, 1)**2), color='blue')
+                ax[1].scatter(2**(np.arange(min_2, max_2,1)), (2**(np.arange(min_2, max_2,1)+1)-1) / (2**(np.arange(min_2, max_2,1)))**2, color='red')
+                ax[1].scatter([3], [3/9], color='orange')
+        
+        ax[0].set_xlim(min_min_d, max_max_d+1)
+        ax[0].set_xlabel('Dimension of Entanglement')
+        ax[0].set_ylabel('Number of Unique Signatures')
+        ax[1].set_xlim(min_min_d, max_max_d+1)
+        ax[1].set_xlabel('Dimension of Entanglement')
+        ax[1].set_ylabel('Percent Distinguished')
+        ax[0].legend(loc='upper left')
+        ax[1].legend(loc='upper right')
+        plt.title('LELM Distinguishability with QFT')
+        plt.tight_layout()
+        plt.savefig(f'output/num_sig_{min_min_d}_{max_max_d}.pdf')
+
+    def combine(subdir):
+        '''Extract results from each file in subdir and combine into one file'''
+        num_sig_d= []
+        min_d = 999
+        max_d = 0
+        subdir = 'output/' + subdir
+        for file in os.listdir(subdir):
+            # in the text file, search for line 'unique states = ' and extract the number
+            d= int(file.split('_')[0])
+            if d < min_d:
+                min_d = d
+            if d > max_d:
+                max_d = d
+            with open(f'{subdir}/{file}', 'r') as f:
+                for line in f:
+                    if 'unique states = ' in line:
+                        num = int(line.split('unique states = ')[1].split('.')[0])
+                        print(line)
+                        print(num)
+                        print(file)
+                        num_sig_d.append([d, num])
+                        break
+        # sort based on d
+        num_sig_d = sorted(num_sig_d, key=lambda x: x[0])
+        # get array of only the number of unique states
+        num_sig_ls = np.array([num for d, num in num_sig_d])
+        # print(num_sig_ls)
+        
+        np.save(f'output/num_sig_{min_d}_{max_d}.npy', num_sig_ls)
+
+    # compute_num_sig(38, 60, parallel=True, display=False)
+    # combine('new')
+    plot([(2, 20), (21, 37)])
 
 
     
