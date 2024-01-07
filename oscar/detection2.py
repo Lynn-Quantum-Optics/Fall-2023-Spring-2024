fc3c231fdf2e38dd392a5bae24b2855ed0fbca86
oscars47 <scholinoscar@gmail.com>
2024-01-07 10:40:58 -0800

    added float in parallel find params

--- oscar/detection2.py
+++ oscar/detection2.py
@@ -5,6 +5,7 @@ from math import factorial
 from scipy import stats
 import matplotlib.pyplot as plt
 from functools import partial
+from concurrent.futures import ProcessPoolExecutor
 from oscars_toolbox.trabbit import trabbit
 
 # ---------- define bell states ------------- #
@@ -213,69 +214,165 @@ def U_guess(params, d):
     # zip together
     qr_ls = list(zip(q_ls, r_ls))
 
+    # print(qr_ls)
+
     # create matrix
-    U = [np.sqrt(q/d)*np.exp(1j*2*np.pi*r/(d)) for q, r in qr_ls]
-    U = np.array(U).reshape((d, d))
-    U = np.kron(U, U)
+    try:
+        U = [np.sqrt(q/d)*np.exp(1j*2*np.pi*r/(d)) for q, r in qr_ls]
+        U = np.array(U).reshape((d, d))
+        U = np.kron(U, U)
+    except RuntimeError:
+        raise ValueError(f'Invalid parameters: {params}')
+    
+    # print(f'U: {U}')
+
     return U
 
-def rand_seq_to_sum(n, size=None):
-    '''Helper func for random_guess to generate a random sequence of length size that sums to n.'''
-    if size is None:
-        size = n # default to n
+# def rand_seq_to_sum(n, size=None):
+#     '''Helper func for random_guess to generate a random sequence of length size that sums to n.'''
+#     if size is None:
+#         size = n # default to n
 
-     # generate a list of random integers
-    numbers = np.random.randint(0, n, size=size) 
+#      # generate a list of random integers
+#     numbers = np.random.randint(0, n, size=size) 
     
-    # adjust sum of list to match n
-    current_sum = sum(numbers)
-    while current_sum != n:
-        for i in range(size):
-            if current_sum < n:
-                increment = min(np.random.randint(0, n), n - current_sum)
-                numbers[i] += increment
-                current_sum += increment
-            elif current_sum > n:
-                decrement = min(numbers[i], current_sum - n)
-                numbers[i] -= decrement
-                current_sum -= decrement
+#     # adjust sum of list to match n
+#     current_sum = sum(numbers)
+#     while current_sum != n:
+#         for i in range(size):
+#             if current_sum < n:
+#                 increment = min(np.random.randint(0, n), n - current_sum)
+#                 numbers[i] += increment
+#                 current_sum += increment
+#             elif current_sum > n:
+#                 decrement = min(numbers[i], current_sum - n)
+#                 numbers[i] -= decrement
+#                 current_sum -= decrement
 
-            if current_sum == n:
-                break
-    return numbers
+#             if current_sum == n:
+#                 break
+#     return numbers
 
-def random_guess(d):
-    '''Gets random integers for q and r.'''
+def rand_seq_to_sum(n, use_int=True):
+    '''Generates random sequence of numbers of size n that sum to n.
+
+    Parameters:
+        :n: number of sum to and size of sequence
+        :use_int: whether to use integers or float. if integers, uses sequential alg
+     
+      '''
+    if use_int:
+        # initialize list
+        seq = [0]*n
+        # generate random sequence of numbers of size n that sum to n
+        for j in range(n):
+            # choose random index
+            i = np.random.randint(0, n+1) # make sure to include n
+            while i + sum(seq) > n:
+                i = np.random.randint(0, n+1) # make sure to include n
+            seq[j] += i
+    else: # use floats. need sum to be n
+         # Generate initial random floats
+        seq = np.random.random(n) * n
+        current_sum = sum(seq)
+
+        precision = 10
+
+        # Iteratively adjust to sum to n
+        while round(current_sum, precision) != n:
+            for i in range(n):
+                if current_sum < n:
+                    increment = min(np.random.random() * (n - current_sum), n - current_sum)
+                    seq[i] += increment
+                    current_sum += increment
+                elif current_sum > n:
+                    decrement = min(seq[i], current_sum - n)
+                    seq[i] -= decrement
+                    current_sum -= decrement
+
+                # Adjust precision to match desired level
+                current_sum = round(current_sum, precision)
+                
+                if current_sum == n:
+                    break
+        # check all positive
+        assert all([s >= 0 for s in seq]), f'Sequence has negative numbers: {seq}'
+
+        # shuffle order
+        np.random.shuffle(seq)
+       
+    return seq
+
+def random_guess(d, use_int=True):
+    '''Gets random integers for q and r.
+
+    Parameters:
+        :d: dimension of the system
+        :use_int: whether to use integers for q and r or float
+    '''
     guess = []
     # q ranges from 0 to d^2 such that sum of q is d^2
     # do this need d times
     for _ in range(d):
-        guess += list(rand_seq_to_sum(d))
+        guess += list(rand_seq_to_sum(d, use_int=use_int))
     # r ranges from 0 to d such that sum of r is d
     # do this need d times
     for _ in range(d):
-        guess += list(rand_seq_to_sum(d))
+        guess += list(rand_seq_to_sum(d, use_int=use_int))
     return guess
 
 def loss(params, d, combinations, ret_c):
     '''Loss function for optimization based on a parametrized get_k function that logs the c and p ls.'''
     U = U_guess(params, d)
     overlap_loss = get_k(d, U, combinations=combinations, ret_c=ret_c)
-    # need to ensure sum of each d length sequence is d
-    sum_loss = 0
-    for i in range(0, len(params), d):
-        sum_loss += np.abs(sum(params[i:i+d]) - d)
-    return overlap_loss + sum_loss
+    # # need to ensure sum of each d length sequence is d
+    # sum_loss = 0
+    # for i in range(0, len(params), d):
+    #     sum_loss += np.abs(sum(params[i:i+d]) - d)
+    # need to ensure unitary
+    unitary_loss = np.linalg.norm(np.eye(d**2) - U @ U.conj().T)
+    return overlap_loss + unitary_loss
 
-def find_params(d, combinations):
-    '''Uses trabbit to find parameters for U.'''
-    random_func = partial(random_guess, d)
+def optimization_task(loss_func, random_func, bounds):
+    return trabbit(loss_func, random_func, bounds=bounds, alpha=0.8, temperature=0.01)
+
+def find_params(d, combinations, use_int=True, parallel=False):
+    '''Uses trabbit to find parameters for U.
+
+    Parameters:
+        :d: dimension of the system
+        :combinations: list of tuples of correlation and phase classes
+        :use_int: whether to use integers for q and r or float
+        :parallel: whether to run in parallel
+    '''
+    random_func = partial(random_guess, d=d, use_int=use_int)
     loss_func = partial(loss, d=d, combinations=combinations, ret_c=False)
     bounds = [(0, d)]*2*d**2
-    x_best, best_loss = trabbit(loss_func, random_func, bounds, alpha=0.8, temperature=0.01)
-    print(f'Best params: {list(x_best)}')
-    print(f'Best loss: {best_loss}')
-    return x_best
+    opt_task = partial(optimization_task, loss_func=loss_func, random_func=random_func, bounds=bounds)
+
+
+    if not parallel:
+        x_best, best_loss = opt_task()
+        print(f'Best params: {list(x_best)}')
+        print(f'Best loss: {best_loss}')
+        return x_best
+    
+    else:
+        # Create a pool of workers using all available CPUs
+        with ProcessPoolExecutor() as executor:  # Defaults to the number of CPUs
+            futures = [executor.submit(opt_task) for _ in range(executor._max_workers)]
+
+            results = []
+            for future in futures:
+                x_best, best_loss = future.result()
+                results.append((x_best, best_loss))
+
+        # Process results
+        best_result = min(results, key=lambda x: x[1])
+        print(f'Best params: {list(best_result[0])}')
+        print(f'Best loss: {best_result[1]}')
+        return best_result[0]
+
 
 # ------ optimize based on given rotations -------- #
 def loss_gr(params, d, combinations, ret_c):
@@ -287,12 +384,11 @@ def find_params_gr(d, combinations):
     '''Uses trabbit to find parameters for U.'''
     random_func = partial(random_params, d)
     loss_func = partial(loss_gr, d=d, combinations=combinations, ret_c=True)
-    bounds = [(0, 2*np.pi)]*d*(d-1) + [(0, 2*np.pi)]*(d-1)
-    x_best, best_loss = trabbit(loss_func, random_func, bounds, alpha=0.8, temperature=0.01)
+    # bounds = [(0, 2*np.pi)]*d*(d-1) + [(0, 2*np.pi)]*(d-1)
+    x_best, best_loss = trabbit(loss_func, random_func, alpha=0.8, temperature=0.01)
     print(f'Best params: {list(x_best)}')
     print(f'Best loss: {best_loss}')
     return x_best
-
 
 # ------ brute force find params -------- #
 def count_partitions(number, max_number):
@@ -324,7 +420,6 @@ def get_total_permutations(d, partitions):
     # Calculate total permutations with zeros included
     partitions = count_partitions(d, d)
     return sum(permutations_with_zeros(partition, d) for partition in partitions)
-
 
 # ------- for display -------- #
 def display_bell(bell, print_bell=True):
@@ -369,8 +464,7 @@ def display_matrix(mat):
 
 if __name__ == '__main__':
     d = 6
-    find_params_gr(d, combinations=[(c, 0) for c in range(d)])
-    # find_params(d, combinations=[(c, 0) for c in range(d)])
+    find_params(d, combinations=[(c, 0) for c in range(d)], use_int=False, parallel=True)
 
 
     # diag = diagonal_phase(d, [np.exp(1j*2*np.pi*i/d) for i in range(1,d)])
